f79915289a39e38cda0201c08d43d44daaf25926
David Löwenfels <282+dfl@users.noreply.github.com>
2025-02-21 17:32:21 +0000

    helper refactor

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,10 +1,10 @@
 module ProjectsHelper
-  def status_color_classes
+  def project_status_color_class
     {
-      'pending' => 'bg-yellow-100 text-yellow-800',
-      'active' => 'bg-green-100 text-green-800',
-      'complete' => 'bg-blue-100 text-blue-800',
-      'archived' => 'bg-gray-100 text-gray-800'
+      "pending" => "bg-yellow-100 text-yellow-800",
+      "active" => "bg-green-100 text-green-800",
+      "complete" => "bg-blue-100 text-blue-800",
+      "archived" => "bg-gray-100 text-gray-800"
     }
   end
 end
