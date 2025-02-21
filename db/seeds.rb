55f639672dc557bf76b0b3d9c62ba0d189e52562
David Löwenfels <282+dfl@users.noreply.github.com>
2025-02-21 21:38:52 +0000

    more realistic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,36 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.find_or_create_by!(email: "user1@example.com", full_name: "User 1") do |user|
-  user.password = "verylongpassword!"
+require 'faker'
+
+password = "1verylongpassword!"
+User.find_or_create_by!(email: "simon@example.com", full_name: "Simon Smith") do |user|
+  user.password = password
+  user.github_username = "simonsmith"
 end
 
-User.find_or_create_by!(email: "user2@example.com", full_name: "User 2") do |user|
-  user.password = "verylongpassword!"
+User.find_or_create_by!(email: "bob@example.com", full_name: "Bob Mackie") do |user|
+  user.password = password
+  user.github_username = "bob"
 end
 
-User.find_or_create_by!(email: "david@example.com", full_name: "David Lowenfels", github_username: "dfl") do |user|
-  user.password = "verylongpassword!"
+User.find_or_create_by!(email: "david@example.com", full_name: "David Lowenfels") do |user|
+  user.password = password
+  user.github_username = "dfl"
 end
 
 project = Project.find_or_create_by!(name: "Project 1")
 
-project.comments.find_or_create_by!(user: User.find_by_full_name("User 1"),
-  text: "This is a comment")
-project.comments.find_or_create_by!(user: User.find_by_full_name("User 2"),
-  text: "This is another comment")
-project.comments.find_or_create_by!(user: User.find_by_full_name("David Lowenfels"),
-  text: "Thanks team!")
+statuses = Project.statuses.keys[1..-1]
+
+10.times do |i|
+  user = User.find(User.pluck(:id).sample)
+  if rand > 0.5
+    project.comments.find_or_create_by! user:,
+      text: Faker::Lorem.sentence, created_at: i.days.ago
+  else
+    Current.session = FactoryBot.create(:session, user:)
+    project.update_attribute(:status, statuses.sample)
+    project.status_changes.last.update_attribute(:created_at, i.days.ago)
+  end
+end
